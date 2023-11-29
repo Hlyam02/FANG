76f32e0f6916b9c18e652048655fec868db84fd9
Hlyam02 <098765432yuuu@sina.com>
2023-11-29 19:01:42 +0900

    09.KIPA

---
+++ 09. KIPA.R
@@ -0,0 +1,137 @@
+
+library(dplyr)
+library(ggplot2)
+
+install.packages("foreign")
+library(foreign)
+
+mental <- read.spss("KIPA_DATA_2021.SAV")
+class(mental)#.객체유형 확인: list
+mental <- as.data.frame(mental)#.데이터프레임으로 변환
+class(mental)#.객체유형 확인: data.frame
+
+
+mental <-mental %>%
+  select(q32_2,q1_4,q32_1,q34_1,q55,d17,d1,d2,ara)%>%
+  rename(suicide=q32_2,
+         satisfaction=q1_4,
+         loneliness=q32_1,
+         family_belief=q34_1,
+         wealth=q55,
+         health=d17,
+         sex=d1,
+         age=d2,
+         area=ara)
+
+str(mental)
+
+table(mental$suicide)
+table(mental$health)
+table(mental$wealth)
+table(mental$satisfaction)
+
+mental$suicide <-as.integer(mental$suicide)
+mental$satisfaction <-as.integer(mental$satisfaction)
+mental$loneliness <-as.integer(mental$loneliness)
+mental$family_belief <-as.integer(mental$family_belief)
+mental$wealth <-as.integer(mental$wealth)
+mental$health <-as.integer(mental$health)
+
+table(mental$suicide)
+table(mental$health)
+table(mental$wealth)
+table(mental$satisfaction)
+
+mental$satisfaction<-mental$satisfaction-1
+mental$wealth<-mental$wealth-1
+
+table(mental$wealth)
+table(mental$satisfaction)
+
+mental$age<-as.character(mental$age)
+mental$sex<-as.character(mental$sex)
+mental$area<-as.character(mental$area)
+
+table(mental$sex)
+table(mental$age)
+table(mental$area)
+
+mental$age<-ifelse(mental$age=="19~29세","20대",mental$age)
+table(mental$age)
+
+summary(mental)
+
+
+#.성별빈도분석
+mental%>%
+  group_by(sex)%>%
+  summarise(n=n())%>% #sex변수의 범주별 빈도 계산
+  mutate(total=sum(n), #sex변수의 빈도 총계
+         pct=round(n/total*100,1)) #sex변수의 범주별 비율
+
+#.연령대별빈도분석
+mental%>%
+  group_by(age)%>%
+  summarise(n=n())%>% #age변수의 범주별 빈도 계산
+  mutate(total=sum(n), #age변수의 빈도 총계
+         pct=round(n/total*100,1)) #age변수의 범주별 비율
+
+#.교차분석
+table(mental$sex, mental$age)
+round(prop.table(table(mental$sex, mental$age),1)*100,1)#교차백분율계산
+chisq.test(mental$sex, mental$age)
+
+#.평균분석
+mental%>%
+  summarise(m1=mean(suicide),m2=mean(satisfaction),
+            m3=mean(loneliness),m4=mean(family_belief),
+            m5=mean(wealth),m6=mean(health))
+
+#.회귀분석:삶의 만족도와 외로움이 자살충동에 미치는 영향
+RA <-lm(data=mental, suicide~satisfaction+loneliness)
+summary(RA)
+
+install.packages("car")
+library(car)
+vif(RA)
+
+#.상관분석:삶의 만족도와 외로움의 상관관계
+cor.test(mental$satisfaction,mental$loneliness)
+
+#.회귀분석:가족신뢰도, 경제안정도, 건강상태가 삶의 만족도에 미치는 영향
+install.packages("ztable")
+library(ztable)
+RA <-lm(data=mental, satisfaction~family_belief+wealth+health)
+summary(RA)
+options(ztable.type="viewer")
+ztable(RA)
+vif(RA)
+
+#.회귀분석:가족신뢰도, 경제안정도, 건강상태가 외로움에 미치는 영향
+RA <-lm(data=mental, loneliness~family_belief+wealth+health)
+summary(RA)
+options(ztable.type="viewer")
+ztable(RA)
+vif(RA)
+
+#.독립표본t검정:성별 삶의 만족도 차이
+t.test(data=mental, satisfaction~sex)
+
+#.연령대별 삶의 만족도 차이
+mental%>%
+  group_by(age)%>%
+  summarise(m=mean(satisfaction))%>%
+  arrange(desc(m))
+
+#.지역별 삶의 만족도 분석과 그래프 작성
+area_satisfaction <-mental%>%
+  group_by(area) %>%
+  summarise(m=mean(satisfaction)) %>%
+  arrange(desc(m))
+
+ggplot(data=area_satisfaction, aes(x=reorder(area,m),y=m))+
+  geom_col()+
+  ggtitle("지역별 만족도")+
+  xlab("지역")+
+  ylab("만족도")+
+  coord_flip()
